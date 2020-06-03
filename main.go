76dd8100abf40ddb1bd2b5a327bae1c538637370
Rangga-PR <ranggaputraraharja@gmail.com>
2020-06-03 19:06:43 +0700

    main

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.Use(cors.Default())
+	log.Fatal(router.Run(":" + os.Getenv("PORT")))
 }
