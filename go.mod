cf8710500d73187f4bfbb159f9fff18020c70b3b
Rangga-PR <ranggaputraraharja@gmail.com>
2020-06-03 21:08:56 +0700

    update user model

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module auth-server-go
 
 go 1.14
+
+require (
+	go.mongodb.org/mongo-driver v1.3.3
+	github.com/gin-contrib/cors v1.3.1
+	github.com/gin-gonic/gin v1.5.0
+	golang.org/x/crypto v0.0.0-20190530122614-20be4c3c3ed5
+)
--- model/user.go
+++ model/user.go
@@ -2,11 +2,12 @@ package model
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
+//User : Define User Document
 type User struct {
-	ID        primitive.ObjectID `json:"id" bson:"_id",omitempty`
+	ID        primitive.ObjectID `json:"id" bson:"_id,omitempty"`
 	Username  string             `json:"username" bson:"username"`
 	Email     string             `json:"email" bson:"email"`
-	Password  string             `json:"password": "password" bson:"password"`
+	Password  string             `json:"password" bson:"password"`
 	UpdatedAt primitive.DateTime `json:"updated_at" bson:"updated_at,omitempty"`
 	CreatedAt primitive.DateTime `json:"created_at" bson:"created_at,omitempty"`
 	DeletedAt primitive.DateTime `json:"deleted_at" bson:"deleted_at,omitempty"`
