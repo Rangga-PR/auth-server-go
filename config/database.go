15d9bde7608268fb01a159aae8294081ce17efaf
Rangga-PR <ranggaputraraharja@gmail.com>
2020-06-03 19:05:49 +0700

    add database config

---
+++ config/database.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+//ConnectDatabase : make connection to mongodb
+func ConnectDatabase(uri string) *mongo.Database {
+	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = client.Connect(context.TODO()); err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), readpref.Primary())
+	if err != nil {
+		log.Fatal("Couldn't connect to the database", err)
+	} else {
+		log.Println("Connected!")
+	}
+
+	return client.Database("auth")
+}
