784abe940b5d5286b25d6bce258d6ea20c03d339
Rangga-PR <ranggaputraraharja@gmail.com>
2020-06-03 19:06:07 +0700

    add user model

---
+++ model/user.go
@@ -0,0 +1,13 @@
+package model
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type User struct {
+	ID        primitive.ObjectID `json:"id" bson:"_id",omitempty`
+	Username  string             `json:"username" bson:"username"`
+	Email     string             `json:"email" bson:"email"`
+	Password  string             `json:"password": "password" bson:"password"`
+	UpdatedAt primitive.DateTime `json:"updated_at" bson:"updated_at,omitempty"`
+	CreatedAt primitive.DateTime `json:"created_at" bson:"created_at,omitempty"`
+	DeletedAt primitive.DateTime `json:"deleted_at" bson:"deleted_at,omitempty"`
+}
